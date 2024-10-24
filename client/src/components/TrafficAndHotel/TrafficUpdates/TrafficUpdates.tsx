01df82bd56af36ed34bf30bcd6b66845bbb9fd3f
Sami Onk <onksami@gmail.com>
2024-10-24 15:06:05 +0200

    refactor: traffic updates

--- client/src/components/TrafficAndHotel/TrafficUpdates/TrafficUpdates.tsx
+++ client/src/components/TrafficAndHotel/TrafficUpdates/TrafficUpdates.tsx
@@ -1,26 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./TrafficUpdates.css";
-<<<<<<< HEAD
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import axios from "axios";
 
 const TrafficUpdates = () => {
   const [positions, setPositions] = useState([]);
-=======
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
-import axios from "axios";
-
-const TrafficUpdates = () => {
-  type Position = [number, number];
-
-  const positionArray: Position[] = [
-    [56.04587001491054, 12.697239762273881],
-    [56.14587001491054, 12.797239762273881],
-    [56.24587001491054, 12.897239762273881],
-  ];
-
-  const [apiData, setApiData] = useState({});
->>>>>>> 7b72255 (refactor: traffic updates)
 
   const url = "https://api.trafikinfo.trafikverket.se/v2/data.json";
   const headers = {
@@ -44,7 +28,7 @@ const TrafficUpdates = () => {
         const trafficFlowData =
           response.data?.RESPONSE?.RESULT?.[0]?.TrafficFlow || [];
 
-        // Gelen data içinden pozisyonları çıkarıyoruz
+        //positions from data
         const positionArray = trafficFlowData
           .map((trafficFlow) => {
             const pointString = trafficFlow.Geometry.WGS84;
